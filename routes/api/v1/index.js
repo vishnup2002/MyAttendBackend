26868886f2300ebf26143a039c1faa8f232ff53d
Vishnu P <vishnupothia@gmail.com>
2022-09-18 20:10:45 +0530

    add test

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.use("/user", require("./user"));
 router.use("/classroom", require("./classroom"));
 router.get("/", passport.authenticate("teacher", { session: false }), test);
-
+router.get("/test", test);
 module.exports = router;
